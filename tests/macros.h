520e54ecccb29ff728d0f0c290bd5684a78c5dd7
Thomas Byr <tbouyer2000@gmail.com>
2021-12-22 15:25:51 +0100

    colors and type cast

--- tests/macros.h
+++ tests/macros.h
@@ -1,27 +1,71 @@
-/*
+/* macro.h
+A simple assertion macro.
 
+*   `assert` macro is used to check the truth evaluation of an expression.
+If the expression evaluates to false, the program will be terminated with an
+error
+*   `assert_eq` macro is used to check the equality of two values.
+This macro, unlike `assert`, tries to cast the values to the same type before
+evaluation. The type of the first argument is arbitrarily chosen.
 */
 
 #ifndef MACROS_H
 #define MACROS_H
 
-#include <assert.h>
+#define assert(expr)                                          \
+    do {                                                      \
+        if (!(expr)) {                                        \
+            fprintf(stderr, "\033[0;31m");                    \
+            fprintf(stderr, "Assertion failed: %s\n", #expr); \
+            abort();                                          \
+        }                                                     \
+    } while (0);
 
-#define assert_eq(a, b) assert(a == b)
-
-#define assert_neq(a, b) assert(a != b)
-
-#define assert_lt(a, b) assert(a < b)
-
-#define assert_gt(a, b) assert(a > b)
-
-#define assert_leq(a, b) assert(a <= b)
-
-#define assert_geq(a, b) assert(a >= b)
+#define assert_eq(a, b)         \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a == _b);       \
+    } while (0);
+#define assert_neq(a, b)        \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a != _b);       \
+    } while (0);
+#define assert_lt(a, b)         \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a < _b);        \
+    } while (0);
+#define assert_gt(a, b)         \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a > _b);        \
+    } while (0);
+#define assert_leq(a, b)        \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a <= _b);       \
+    } while (0);
+#define assert_geq(a, b)        \
+    do {                        \
+        __typeof__(a) _a = (a); \
+        __typeof__(a) _b = (b); \
+        assert(_a >= _b);       \
+    } while (0);
 
 #define test_case(name)                                         \
-    printf("\nrunning %s:%d: %s\n", __FILE__, __LINE__, #name); \
-    name();                                                     \
-    printf("\n");
+    do {                                                        \
+        printf("\033[0m");                                      \
+        printf("running %s:%d: %s", __FILE__, __LINE__, #name); \
+        name();                                                 \
+        printf("\033[0;32m");                                   \
+        printf(" ok\n");                                        \
+        printf("\033[0m");                                      \
+    } while (0)
 
 #endif
