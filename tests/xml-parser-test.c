e26c08fb7b821a410881799016f4699d6eb13d62
Thomas Byr <tbouyer2000@gmail.com>
2021-12-22 18:02:26 +0100

    i'm lost send help

--- tests/xml-parser-test.c
+++ tests/xml-parser-test.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "macros.h"
+#include "protocol.h"
 #include "types.h"
 #include "xml-parser.h"
 
@@ -15,4 +16,23 @@ void xml_test_error_0(void) {
     assert_eq(err, PARSER_OK);
 }
 
-void xml_parser_test() { test_case(xml_test_error_0); }
+void xml_test_0(void) {
+    char arr[] = "abcdefghijklmnopqrstuvwxyz";
+    int count = 0;
+    foreach (item in arr) {
+        (void)item;
+        count++;
+    }
+    assert_eq(count, 27);
+}
+
+void xml_test_1(void) {
+    int i = 0;
+    (void)i;
+}
+
+void xml_parser_test() {
+    test_case(xml_test_error_0);
+    test_case(xml_test_0);
+    test_case(xml_test_1);
+}
