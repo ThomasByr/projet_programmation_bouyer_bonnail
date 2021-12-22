6dc4848d93f3fccecc85bce8006707572fec2b6e
Thomas Byr <tbouyer2000@gmail.com>
2021-12-22 20:57:43 +0100

    more

--- includes/protocol.h
+++ includes/protocol.h
@@ -4,7 +4,7 @@
 This is used to iterate over an unidimensional array. Note that `char arr[] =
 ...;` will be iterated over properly but `char *arr = ...;` will not.
 *   `lerp` macro is used to "move" a value from one range to another.
-Given values are casted to the same type before evaluation.
+It is the caller's responsability to cast the values to the same type.
 
 */
 
@@ -28,14 +28,7 @@ Given values are casted to the same type before evaluation.
 #define CAT_HELPER(a, b) a##b
 #define X(name) CAT(__##name, __LINE__)
 
-#define lerp(x, x0, x1, y0, y1)   \
-    do {                          \
-        __typeof__(x) _x = (x);   \
-        __typeof__(x) _x0 = (x0); \
-        __typeof__(x) _x1 = (x1); \
-        __typeof__(x) _y0 = (y0); \
-        __typeof__(x) _y1 = (y1); \
-    } while (0);                  \
-    (((_y0) * ((_x1) - (x)) + (_y1) * ((x) - (_x0))) / ((_x1) - (_x0)))
+#define lerp(x, x0, x1, y0, y1) \
+    (((y0) * ((x1) - (x)) + (y1) * ((x) - (x0))) / ((x1) - (x0)))
 
 #endif
