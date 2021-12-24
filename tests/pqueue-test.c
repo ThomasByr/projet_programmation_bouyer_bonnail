40969884817b64ccdd879c136ba6fda45785fdb3
Thomas Byr <tbouyer2000@gmail.com>
2021-12-24 16:59:10 +0100

    use of auto-cast

--- tests/pqueue-test.c
+++ tests/pqueue-test.c
@@ -12,13 +12,13 @@ void pqueue_test_0(void) {
     pqueue_push(pq, (void *)"bob3", 3);
     assert_eq(pqueue_size(pq), 3);
 
-    assert_eq(pqueue_find_min(pq), (void *)"bob1");
+    assert_eq(pqueue_find_min(pq), "bob1");
     pqueue_decrease_key(pq, (void *)"bob3", 0);
-    assert_eq(pqueue_find_min(pq), (void *)"bob3");
+    assert_eq(pqueue_find_min(pq), "bob3");
 
     char *min = (char *)pqueue_pop_min(pq);
     assert_eq(strcmp(min, "bob3"), 0);
-    assert_eq(pqueue_find_min(pq), (void *)"bob1");
+    assert_eq(pqueue_find_min(pq), "bob1");
     assert_eq(pqueue_size(pq), 2);
 
     pqueue_free(pq);
