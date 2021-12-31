2d1111bf860184d99deea96a9bf00862c9635f17
Thomas Byr <tbouyer2000@gmail.com>
2021-12-31 19:17:59 +0100

    getting there

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "protocol.h"
 #include "xml-parser.h"
 
 int main(int argc, char *argv[]) {
@@ -8,6 +9,8 @@ int main(int argc, char *argv[]) {
     (void)argv;
 
     parser_info_t *info = parser_info_new();
+
+    print_version();
 
     char filename[] = "assets/dblp.xml";
     parser_error_type_t err = parse(filename, info);
