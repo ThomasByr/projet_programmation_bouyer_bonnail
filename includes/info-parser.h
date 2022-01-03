4358316d48c879cf4fd733b5d5dad4549ce20ca4
Thomas Byr <tbouyer2000@gmail.com>
2022-01-03 19:17:03 +0100

    correction des conflits + création des .h

--- includes/info-parser.h
+++ includes/info-parser.h
@@ -5,16 +5,14 @@
 #ifndef INFO_PARSER_H
 #define INFO_PARSER_H
 
-struct parser_context_s
-{
+struct parser_context_s {
     int text_count;
     int open_count;
     int close_count;
 };
 typedef struct parser_context_s parser_context_t;
 
-enum parser_error_type_e
-{
+enum parser_error_type_e {
     PARSER_OK,
     ERROR_UNABLE_TO_ALLOCATE_MEMORY,
     ERROR_WHILE_READING_FILE,
@@ -23,8 +21,7 @@ enum parser_error_type_e
 };
 typedef enum parser_error_type_e parser_error_type_t;
 
-struct parser_info_s
-{
+struct parser_info_s {
     void (*handleOpenTag)(char *, void *);
     void (*handleCloseTag)(char *, void *);
     void (*handleText)(char *, void *);
@@ -50,46 +47,31 @@ void handleOpenTag(char *tag, void *data);
 
 /**
  * @brief tell the parser to close a tag
-<<<<<<< HEAD
- * 
-=======
  *
+ * @param tag tag name (without the <> brackets and any attributes)
+ * @param data the data to pass to the callback functions
  */
+void handleCloseTag(char *tag, void *data);
 
 /**
  * @brief new parser info structure
-<<<<<<< HEAD
- * 
- * @return parser_info_t* 
-=======
  *
  * @return parser_info_t*
->>>>>>> 7c92f791ee13737df0c25425f9c58acd01c9783c
  */
 parser_info_t *parser_info_new(void);
 
 /**
  * @brief free a parser info underlaying structure
-<<<<<<< HEAD
- * 
-=======
  *
->>>>>>> 7c92f791ee13737df0c25425f9c58acd01c9783c
  * @param info parser info to free
  */
 void parser_info_free(parser_info_t *info);
 
 /**
  * @brief transform an error thrown by the parser into a string
-<<<<<<< HEAD
- * 
- * @param error error code
- * @return char* 
-=======
  *
  * @param error error code
  * @return char*
->>>>>>> 7c92f791ee13737df0c25425f9c58acd01c9783c
  */
 char *parser_error_type_to_string(parser_error_type_t error);
 
