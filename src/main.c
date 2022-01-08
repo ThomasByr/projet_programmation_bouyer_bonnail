d60ccabee31469612eae341acf39b899dd1b8b97
Thomas Byr <tbouyer2000@gmail.com>
2022-01-08 18:41:56 +0100

    this is starting to look like something

--- src/main.c
+++ src/main.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "args-parser.h"
 #include "protocol.h"
 #include "xml-parser.h"
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
-
-    parser_info_t *info = parser_info_new();
-
-    print_version();
-    printf("\n");
-    print_usage();
-    return 0;
-
-    char filename[] = "assets/dblp.xml";
-    parser_error_type_t err = parse(filename, info);
-    parser_info_free(info);
-    printf("%s\n", parser_error_type_to_string(err));
+    options_t *options = options_new();
+    parse_args(argc, argv, options);
+    options_free(options);
 }
