357fc6b0cc4bd52911cec264fad9c6c1663479ee
Thomas Byr <tbouyer2000@gmail.com>
2022-01-19 19:05:17 +0100

    parsing the file

--- src/io.c
+++ src/io.c
@@ -21,12 +21,30 @@
 
 int exec(options_t *options) {
     parser_info_t *info = parser_info_new();
-    parse(options->input_file, info);
+
+    int flag = options->quiet == 1 ? 2 : options->verbose == 1 ? 1 : 0;
+    parser_error_type_t err = parse(options->input_file, info, flag);
+
+    int rv = 0;
+    switch (err) {
+    case PARSER_OK:
+        if (options->verbose == 1) {
+            char *msg = parser_error_type_to_string(err);
+            award(msg);
+        }
+        break;
+    default:
+        rv = 1;
+        if (options->quiet == 0) {
+            char *msg = parser_error_type_to_string(err);
+            alert(msg);
+        }
+        break;
+    }
 
     hset_t *nodes = info->context->nodes;
-    (void)nodes;
     parser_info_free(info); // does not free nodes info->context->nodes
 
     hset_free(nodes); // at the end
-    return 0;
+    return rv;
 }
